8fb59dc093adda664ae7562532d82e2d355f16e3
hieptrinh96 <xmikecolx@gmail.com>
2025-01-23 11:09:01 -0600

    Update yml page and add generateStaticParams function

--- frontend/src/app/form/[slug]/page.jsx
+++ frontend/src/app/form/[slug]/page.jsx
@@ -3,6 +3,13 @@ import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
 import ClientForm from "../ClientForm"
 
+export async function generateStaticParams() {
+  const slugs = Object.keys(formConfigurations)
+  return slugs.map((slug) => ({
+    slug: slug
+  }))
+}
+
 export default async function FormPage({ params }) {
   const { slug } = await params
   console.log("Received slug: ", slug)
