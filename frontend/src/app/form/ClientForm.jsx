994231e540cdd292c7ec3e4efaedb1d50cb476e7
hieptrinh96 <xmikecolx@gmail.com>
2025-01-24 12:32:06 -0600

    Add handle submit function to form

--- frontend/src/app/form/ClientForm.jsx
+++ frontend/src/app/form/ClientForm.jsx
@@ -4,57 +4,106 @@ import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row"
+
 const ClientForm = ({ config }) => {
-  const [formData, setFormData] = useState({})
+  const [formData, setFormData] = useState({
+    'fullName': '',
+    'email': '',
+    'stockNumber': 0,
+    'itemDescription': '',
+    'quantity': 0
+  })
   const handleChange = e => {
     const { name, value } = e.target
     setFormData(prevData => ({
       ...prevData,
       [name]: value
     }))
-
-    const handleSubmit = e => {
-      e.preventDefault()
-    }
   }
+    const handleSubmit = async (e) => {
+      e.preventDefault()
+      try {
+        const res = await fetch('/api/populate-pdf', {
+          method: 'post',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify(formData)
+        })
+        setFormData({
+          'fullName': '',
+          'email': '',
+          'stockNumber': 0,
+          'itemDescription': '',
+          'quantity': 0
+        })
+        return res
+      } catch (e) {
+        console.error('Error = ', e)
+      }
+    }
 
   return ( 
     <div className="container">
       <div className="row justify-content-center">
         <div className="col-md-6">
           <h1 className="text-center mb-4">{config.title}</h1>
-            <Form>
+            <Form onSubmit={handleSubmit}>
               <Row className="mb-3">
                 <Form.Group as={Col}>
                   <Form.Label>Full Name</Form.Label>
-                  <Form.Control type="text">
+                  <Form.Control
+                    type="text"
+                    name="fullName"
+                    value={formData.fullName}
+                    onChange={handleChange}
+                    >
                   </Form.Control>
                 </Form.Group>
 
                 <Form.Group as={Col}>
                   <Form.Label>Email</Form.Label>
-                  <Form.Control></Form.Control>
+                  <Form.Control
+                    type="email"
+                    name="email"
+                    value={formData.email}
+                    onChange={handleChange}>
+                    </Form.Control>
                 </Form.Group>
               </Row>
-              {config.fields.slice(2, config.fields.length).map((field, index) => (
-                <Form.Group controlId={field.name} key={field.name} className="mb-3">
-                <Form.Label>{field.label}</Form.Label>
-                  {index === 1 ? (
-                    <Form.Control
-                      as="textarea"
-                      name={field.name}
-                      required={field.required}
-                      rows="3" />
-                  ) : (
-                    <Form.Control
-                      type={field.type}
-                      name={field.name}
-                      required={field.required} />
-                  )}
+              <Row className="mb-3">
+                <Form.Group as={Col}>
+                  <Form.Label>Stock Number</Form.Label>
+                  <Form.Control
+                    type="number"
+                    name="stockNumber"
+                    required={true}
+                    value={formData.stockNumber}
+                    onChange={handleChange} />
                 </Form.Group>
-                ))}
+
+                <Form.Group as={Col}>
+                  <Form.Label>Quantity</Form.Label>
+                  <Form.Control 
+                    type="number"
+                    name="quantity"
+                    required={true}
+                    value={formData.quantity}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+              </Row>
+
+              <Form.Group className="mb-3">
+                <Form.Label>Item Description</Form.Label>
+                <Form.Control 
+                  as='textarea'
+                  name="itemDescription"
+                  required={true}
+                  value={formData.itemDescription}
+                  rows='3'
+                  onChange={handleChange} />
+              </Form.Group>
               <div className="d-grid">
-                <Button variant="primary" type="submit">Submit</Button>
+                <Button variant="primary" type="submit" onSubmit={handleSubmit}>Submit</Button>
               </div>
             </Form>
         </div>
--- frontend/src/app/form/formConfigurations.js
+++ frontend/src/app/form/formConfigurations.js
@@ -2,10 +2,10 @@ export const formConfigurations = {
   formDA: {
     title: "DA Form",
     fields: [
-      { label: "Full Name", name: "full name", type: "text", required: true },
+      { label: "Full Name", name: "fullName", type: "text", required: true },
       { label: "Email", name: "email", type: "text", required: true },
-      { label: "Stock Number", name: "stock number", type: "number", required: true },
-      { label: "Item Description", name: "item description", type: "text", required: true },
+      { label: "Stock Number", name: "stockNumber", type: "number", required: true },
+      { label: "Item Description", name: "itemDescription", type: "text", required: true },
       { label: "Quantity", name: "quantity", type: "number", required: true }
     ]
   }
